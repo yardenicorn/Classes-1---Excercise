6436bf10d930c848e46dc3c92aaba1d4e514172f
Yarden Admon <yardenadmon@gmail.com>
2023-03-13 13:25:08 +0200

    the software is done
    all functions work
    software is working

--- Pizza.cs
+++ Pizza.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 public class Pizza
 {
-    public int basePrice { get; private set; } = 20;
-    public List<Topping> Toppings { get; private set; }
+    // what is the convention for constant value in classes?
+    public const int basePrice = 20;
+    public List<Topping> Toppings { get; private set; } = new List<Topping>();
     public bool isVegan { get; private set; }
 
     public Pizza(bool isVegan)
@@ -15,23 +16,25 @@ public class Pizza
         this.isVegan = isVegan;
     }
 
+    // needs to be a list of constant toppings, how do I do this?
     List<Topping> ToppingsMenu = new List<Topping>
-            {
-            new Topping("extra cheese", 1, false),
-            new Topping("green olives", 1, true),
-            new Topping("black olives", 1, true),
-            new Topping("mushrooms", 1, true),
-            new Topping("onions", 1, true),
-            new Topping("garlic", 1, true),
-            new Topping("corn", 1, true),
-            new Topping("pepperoni", 5, false),
-            new Topping("tuna", 5, false),
-            new Topping("ham", 5, false),
-            new Topping("goomba sauce", 3, true),
-            new Topping("soy cheese", 3, true),
-            new Topping("vegan meat", 3, true),
-            };
+    {
+         new Topping("extra cheese", 5, false),
+         new Topping("green olives", 3, true),
+         new Topping("black olives", 3, true),
+         new Topping("mushrooms", 3, true),
+         new Topping("onions", 3, true),
+         new Topping("garlic", 3, true),
+         new Topping("corn", 3, true),
+         new Topping("pepperoni", 5, false),
+         new Topping("tuna", 5, false),
+         new Topping("ham", 5, false),
+         new Topping("goomba sauce", 5, true),
+         new Topping("soy cheese", 10, true),
+         new Topping("vegan meat", 10, true),
+    };
 
+    // prints the optional toppings for the user to choose, according to the pizza he had chosen
     public void PrintToppingMenu()
     {
         for (int i = 0; i < ToppingsMenu.Count; i++)
@@ -44,13 +47,56 @@ public class Pizza
         }
     }
 
-    public void CreatePizza(List<Topping> userToppings)
+    // adding the toppings the user chose on his pizza.
+    // should I use another name for the function?
+    public void CreatePizza()
     {
-
+        int topping = UserInputToppingIndex();
+        while (topping != 0)
+        {
+            Toppings.Add(ToppingsMenu[topping]);
+            topping = UserInputToppingIndex();
+        }
+        YourOrder();
     }
 
-    public void PizzaPrice()
+    // checks if the user's input for topping is legit. if yes - it returns the topping.
+    // should I use another name for the function?
+    private int UserInputToppingIndex()
     {
+        bool IsNumeric = Int32.TryParse(Console.ReadLine(), out int topping);
 
+        // the loop needs to check that a number has been inserted, and not something else, or if the number is within the list's range or not a negative number.
+        // the loop checks if the user's input is not legit.
+        while (!IsNumeric || topping > ToppingsMenu.Count || topping < 0)
+        {
+            Console.WriteLine("Please enter valid numbers only");
+            IsNumeric = Int32.TryParse(Console.ReadLine(), out topping);
+        }
+        return topping;
+    }
+
+    // calculate the pizza price according to the toppings that the user has chose
+    public int PizzaPrice()
+    {
+        int pizzaPrice = basePrice;
+        foreach (Topping t in Toppings)
+        {
+            pizzaPrice += t.Price;
+        }
+        return pizzaPrice;
+    }
+
+    // print to the screen the user's order with total price
+    public void YourOrder()
+    {
+        Console.WriteLine("Your Order:");
+        Console.WriteLine($"Base price for pizza: {basePrice}");
+        foreach (Topping t in Toppings)
+        {
+            Console.WriteLine($"{t.Name}, Price: {t.Price}");
+        }
+        Console.WriteLine($"Total: {PizzaPrice()}");
+        Console.WriteLine("Thank you for buying at The Unicorn Pizza!");
     }
 }
--- PizzaSoftware.cs
+++ PizzaSoftware.cs
@@ -13,21 +13,19 @@ public class PizzaSoftware
         Console.WriteLine("Hello and Welcome to The Unicorn Pizza!");
         Console.WriteLine("Would you like: \n 1. Vegan Pizza \n 2. Non Vegan Pizza \n Please enter valid numbers only");
 
-        bool IsNumeric = Int32.TryParse(Console.ReadLine(), out int userInput);
+        bool IsNumeric = Int32.TryParse(Console.ReadLine(), out int pizzaType);
 
-        while (!IsNumeric || (userInput != 1 && userInput != 2))
+        while (!IsNumeric || (pizzaType != 1 && pizzaType != 2))
         {
             Console.WriteLine("Please enter valid numbers only");
-            IsNumeric = Int32.TryParse(Console.ReadLine(), out userInput);
+            IsNumeric = Int32.TryParse(Console.ReadLine(), out pizzaType);
         }
 
-        bool isVegan = (userInput == 1) ? true : false;
+        bool isVegan = (pizzaType == 1) ? true : false;
         Pizza newPizza = new Pizza(isVegan);
         Console.WriteLine("OK! These are your options for toppings! \n Please enter the numbers of toppings you would like to add to your pizza. when your'e done - write 0");
         newPizza.PrintToppingMenu();
-
-        
-
+        newPizza.CreatePizza();
         Console.ReadKey();
     }
 }
